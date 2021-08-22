bc0a76dc8b0e4ec1e4c138b0624033371fc82521
timor.rust <75886852+timorust@users.noreply.github.com>
2021-08-22 16:28:26 +0300

    test GET POSTi

--- app.js
+++ app.js
@@ -23,4 +23,9 @@ app.get('/add/', async(req, res) => {
   res.status(200).json(result);
 })
 
+app.post('/add_post/', async(req, res) => {
+  const result = calc.add(Number(req.body.x), Number(req.body.y))
+  res.status(201).json(result)
+})
+
 app.listen(port, () => console.log(`Listening to port ${port}`));
--- app.test.js
+++ app.test.js
@@ -5,9 +5,10 @@ const server = supertest.agent('localhost:8080')
 
 describe('testing REST api', () => {
   it('test GET', (done) => {
-    server.get('/add?x=365&y=235').
-    expect('Content-type', /json/).
-    expect(200).
+    server
+    .get('/add?x=365&y=235')
+    .expect('Content-type', /json/)
+    .expect(200).
     end((err, res) => {
       res.status.should.equal(200) // assert
       console.log(res.body)
@@ -15,4 +16,16 @@ describe('testing REST api', () => {
       done();
     })
   })
-})
+  it('testing POST', function(done) {
+    server
+    .post('/add_post')
+    .send({x : 15, y : 40})
+    .expect('Content-type', /json/)
+    .expect(201)
+    .end(function(err, res) {
+      res.status.should.equal(201);
+      res.body.should.equal(55);
+      done();
+    })
+  })
+});
